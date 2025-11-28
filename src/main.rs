9a56c84b98eae27359ffe71299fe2b823e80da81
Ghost <duchojakub987@gmail.com>
2025-11-28 08:43:51 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut args: Vec<String> = env::args().collect();
+
+    args.remove(0);
+
+    parse_args(args);
+
+}
+
+fn parse_args(args: Vec<String>) {
+    println!("{:?}", args);
 }
